2a124bb899325e5ecf0a1922bbf62df1923b68e7
thepaganrabbit <gonzalesgustavoc@gmail.com>
2022-06-01 10:48:20 -0600

    chore: added cypress e2e and fixed jest. Switched to yarn.

---
+++ cypress/e2e/Home.spec.cy.js
@@ -0,0 +1,5 @@
+describe('Home.spec.cy.js', () => {
+  it('should visit', () => {
+    cy.visit('/')
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "testEnvironment": "jsdom"
-}
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
