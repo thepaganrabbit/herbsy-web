ffc27f2400ffc973fca0fdd148a6f6bc6c0b50b8
thepaganrabbit <gonzalesgustavoc@gmail.com>
2022-06-01 11:17:09 -0600

    chore: switched to yarn. E2E tested cabinet.

--- cypress/e2e/home.spec.cy.js
+++ cypress/e2e/home.spec.cy.js
@@ -1,8 +1,14 @@
 describe('home.spec.cy.js', () => {
   it('should visit', () => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
   it('should show the side menu', () => {
-    cy.get('[data-testid="Side Navigation Menu"]').should('be.visible')
-  })
-})
+    cy.visit('/');
+    cy.get('[data-testid="Side Navigation Menu"]').should('be.visible');
+  });
+  it('should go to /cabinet', () => {
+    cy.visit('/');
+    cy.get('[data-testid="Cabinet button"]').should('be.visible').click();
+    cy.get('[data-testid="Cabinet Title"]').should('be.visible');
+  });
+});
--- src/pages/Cabinet/Cabinet.jsx
+++ src/pages/Cabinet/Cabinet.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import SideNav from '../../components/SideMenu/SideMenu';
 
 const Cabinet = () => {
-  return <div><SideNav />Cabinet</div>;
+  return (
+    <div>
+      <SideNav />
+      <div className="cabinet-container">
+        <div className="title">
+          <h2 data-testid="Cabinet Title">Cabinet</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cabinet;
